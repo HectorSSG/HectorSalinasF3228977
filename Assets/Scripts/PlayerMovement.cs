025595768b9c6639ef79eca8b11e5289acc72da9
Hector <hectorsalinasgalindo@gmail.com>
2025-12-05 21:48:24 -0500

    5/12/2025

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,8 +2,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float horizontalMovement;
+    private float horizontalMovement;
+    [SerializeField] private float jumpForce;
     [SerializeField] private float speed;
+    [SerializeField] private bool isGround;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float radiusGround;
     private Rigidbody2D rigidBody;
     void Start()
     {
@@ -12,5 +17,19 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         horizontalMovement = Input.GetAxis("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space) && isGround == true)
+        {
+            rigidBody.linearVelocity = new Vector2(rigidBody.linearVelocityX, jumpForce);
+        }
+    }
+    private void FixedUpdate()
+    {
+        rigidBody.linearVelocity = new Vector2(horizontalMovement * speed, rigidBody.linearVelocityY);
+        isGround = Physics2D.OverlapCircle(groundCheck.position, radiusGround, groundLayer);
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(groundCheck.position, radiusGround);
     }
 }
